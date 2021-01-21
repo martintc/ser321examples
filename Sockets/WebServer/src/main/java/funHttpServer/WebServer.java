5a54e10548d76447ff5b2d6f73c9b6f22ab1679b
toddmartin <kj4ntv@gmail.com>
2021-01-20 23:31:43 -0800

    ERROR CHECKING MULTIPLY Error checking was added for inapprorpriate parameters on a request for multiple

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -202,17 +202,26 @@ class WebServer {
           query_pairs = splitQuery(request.replace("multiply?", ""));
 
           // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+          try {
 
-          // do math
-          Integer result = num1 * num2;
+            Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+            Integer num2 = Integer.parseInt(query_pairs.get("num2"));
 
-          // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
+            // do math
+            Integer result = num1 * num2;
+
+            // Generate response
+            builder.append("HTTP/1.1 200 OK\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("Result is: " + result);
+          } catch (NumberFormatException e) {
+            builder.append("HTTP/1.1 400 OK\n");
+            builder.append("Content-type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("Error: An input was not a valid number\n");
+
+          }
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
